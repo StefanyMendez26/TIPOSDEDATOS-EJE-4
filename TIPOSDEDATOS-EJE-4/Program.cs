6d470d79d32203bfaafeff73a1e3a354f7cf397c
DANIELA STEFANY MENDEZ CRUZ <mc22i04003@usonsonate.edu.sv>
2025-02-14 14:35:59 -0600

    Agregar archivos de proyecto.

---
+++ TIPOSDEDATOS-EJE-4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("!");
+//Ejercicio 4:
+//Crea un programa que declare una variable de tipo bool y le asigne true o false. Usa una instrucción if para evaluar
+//    y mostrar en consola el resultado de una condición relacionada con esa variable (true o false) y diga en un mensaje 
+//    que el Ing Daro 
+
+Console.WriteLine("Ejercicio 4!");
+
+bool variable1 = true;
+
+if (variable1)
+{
+    Console.WriteLine("Ingrwso a la condicion");
+
+}
