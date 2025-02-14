254e98265a3e765cf4369a8e8f1869e534404f60
DANIELA STEFANY MENDEZ CRUZ <mc22i04003@usonsonate.edu.sv>
2025-02-14 14:36:42 -0600

    ""

--- TIPOSDEDATOS-EJE-4/Program.cs
+++ TIPOSDEDATOS-EJE-4/Program.cs
@@ -2,9 +2,7 @@
 Console.WriteLine("!");
 //Ejercicio 4:
 //Crea un programa que declare una variable de tipo bool y le asigne true o false. Usa una instrucción if para evaluar
-//    y mostrar en consola el resultado de una condición relacionada con esa variable (true o false) y diga en un mensaje 
-//    que el Ing Daro 
-
+//    y mostrar en consola el resultado de una condición relacionada con esa variable (true o false) 
 Console.WriteLine("Ejercicio 4!");
 
 bool variable1 = true;
